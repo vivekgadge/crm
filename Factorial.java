35a44443bbe9e98ca9421baf397ef6965467f23e
vivekgadge <125717195+vivekgadge@users.noreply.github.com>
2023-02-21 13:11:49 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+class Factorial
+{
+	public static void main(String [] args)
+	{
+		int n ,i,a=1;
+		n = 5;
+		
+		for(i=1; i<=5; i++)
+		{
+			a = a*i;
+
+		}
+		System.out.println("Factorial of the given number is:" + a);
+
+	}
+
+
+
+
+}
